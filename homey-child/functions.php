6e114b536591628431beb3cdf0a643d459758094
Customer <u51-mygp4nivg3dy@c1110269.sgvps.net>
2025-12-09 12:41:20 +0000

    add child theme

---
+++ homey-child/functions.php
@@ -0,0 +1,11 @@
+<?php
+function homey_enqueue_styles() {
+    
+    // enqueue parent styles
+    wp_enqueue_style('homey-parent-theme', get_template_directory_uri() .'/style.css');
+    
+    // enqueue child styles
+    wp_enqueue_style('homey-child-theme', get_stylesheet_directory_uri() .'/style.css', array('homey-parent-theme'));
+}
+
+add_action('wp_enqueue_scripts', 'homey_enqueue_styles');
